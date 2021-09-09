a492c7eb9f6b4b289e339bb747269dfd2edd6ac0
BekirGeris <48486272+BekirGeris@users.noreply.github.com>
2021-09-09 13:47:47 +0300

    initial commit

---
+++ Top Down Shooter/Assets/TopDownShooter/Scripts/Camera/CameraSettings.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownShooter
+{
+    [CreateAssetMenu(menuName = "TopDown Shooter/Camera/Camera Settings")]
+    public class CameraSettings : ScriptableObject
+    {
+        [Header("Rotation")]
+        [SerializeField] private float _rotationLerpSpeet = 1;
+        public float RotationLerpSpeet { get { return _rotationLerpSpeet; } }
+
+        [Header("Rotation")]
+        [SerializeField] private Vector3 _positionOffset;
+        public Vector3 PositionOffset { get { return _positionOffset; } }
+
+        [SerializeField] private float _positionLerp = 1;
+        public float PositionLerp { get { return _positionLerp; } }
+    }
+}
